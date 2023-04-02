7d384627463ca2777122535476fac52f3c591a17
Bahadır Bolat <87010618+bahadirbolatt@users.noreply.github.com>
2023-04-02 00:12:31 +0000

    emres

---
+++ ex08/blackjack.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int ac, char **av)
+{
+    int i = 0;
+    char *chars = "23456789TJDKA";
+    
+    if(ac == 2)
+    {
+        if(strcmp(&av, *chars))
+        {
+            
+        }
+    }
+}
